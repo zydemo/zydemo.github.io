569656220dac3f887d8987a53d286e4eae87dcd2
zydemo <564359804@163.com>
2019-02-25 23:11:02 +0800

    20190225修改生成md文件代码

--- createNew_MDFile.py
+++ createNew_MDFile.py
@@ -26,6 +26,12 @@ class Create_newMD:
         self.date_patt = re.compile(r'\d{4}-\d{1,2}-\d{1,2}')
         # 时间格式规则
         self.time_patt = re.compile(r'\d{1,2}:\d{1,2}:\d{1,2}')
+        # 存储所有文件的分类名
+        self.categories_list = []
+        # 存储所有文件的分类名
+        self.tags_list = []
+
+
 
     def create(self):
         self.path = "./_posts"
@@ -36,6 +42,24 @@ class Create_newMD:
         else:
             # 先获取_posts目录下的所有文件
             filelist = os.listdir(self.path)
+            for file in filelist:
+                with open(self.path+"/"+file,'r',encoding='utf-8')as f:
+                    while True:
+                        content = f.readline()
+                        # 获取分类
+                        if "categories: " in content:
+                            category_ = content.split("categories: ")[1].strip().replace("\n","")
+                            self.categories_list.append(category_)
+                        # 获取标签
+                        if "tag: " in content:
+                            tag_ = content.split("tag: ")[1].strip().replace("\n","")
+                            self.tags_list.append(tag_)
+                        else:
+                            if not content:
+                                break
+            all_categories = list(set(self.categories_list)) # 去掉重复的名字
+            all_tags = list(set(self.tags_list))
+
             while True:
                 input_date = input("\n" + "文章发布日期(格式2019-02-21,不输入默认为当前日期):").strip()
                 re_date = self.date_patt.findall(input_date)
@@ -66,24 +90,66 @@ class Create_newMD:
                 else:
                     print("\n" + "标题不能为空或只为空格，请重新输入！")
                     continue
+            # while True:
+            #     input_category = input("\n" + "文章分类名:").strip()
+            #     if input_category != "":
+            #         # 如果输入的是纯数字，要加上双引号:"2"
+            #         if input_category.isdigit():
+            #             category = "categories: " + '"' + input_category + '"'
+            #         else:
+            #             category = "categories: " + input_category
+            #         break
+            #     else:
+            #         print("\n" + "分类名不能为空或只为空格，请重新输入！")
+            #         continue
+            #
+            # input_tag = input("\n" + "文章标签名(不输入默认为分类名):").strip()
+            # if input_tag != "":
+            #     tag = "tag: " + input_tag
+            # else:
+            #     tag = "tag: " + input_category
+            print("===========已添加过的分类名如下===========")
+            print("{:^15}\t{:^10}".format("分类代号", "分类名称"))
+            categories_dic = {}
+            for i, chr in enumerate(all_categories):
+                print("{:^18}\t{:^12}".format(str(i + 1) + "-", chr))
+                categories_dic[str(i + 1) + "-"] = chr
+            # print(categories_dic)
             while True:
-                input_category = input("\n" + "文章分类名:").strip()
+                input_category = input("\n" + "输入文章分类名(可输入相应分类代号，注意后面的减号):").strip()
                 if input_category != "":
                     # 如果输入的是纯数字，要加上双引号:"2"
                     if input_category.isdigit():
                         category = "categories: " + '"' + input_category + '"'
+                    elif input_category in categories_dic.keys():
+                        category = "categories: " + categories_dic[input_category]
                     else:
                         category = "categories: " + input_category
                     break
                 else:
                     print("\n" + "分类名不能为空或只为空格，请重新输入！")
                     continue
+            print("分类名为：%s"%category.split("categories: ")[1])
 
-            input_tag = input("\n" + "文章标签名(不输入默认为分类名):").strip()
+            print("\n" + "===========已添加过的标签名如下===========")
+            print("{:^15}\t{:^10}".format("标签代号", "标签名称"))
+            tags_dic = {}
+            for j, chr in enumerate(all_tags):
+                print("{:^18}\t{:^12}".format(str(j + 1) + "-", chr))
+                tags_dic[str(j + 1) + "-"] = chr
+            input_tag = input("\n" + "输入文章标签名(不输入默认为分类名或输入相应标签代号，注意后面的减号):").strip()
             if input_tag != "":
-                tag = "tag: " + input_tag
+                if input_tag in tags_dic.keys():
+                    tag = "tag: " + tags_dic[input_tag]
+                else:
+                    tag = "tag: " + input_tag
             else:
-                tag = "tag: " + input_category
+                if input_category.isdigit():
+                    tag = "tag: " + input_category
+                else:
+                    tag = "tag: " + category.split("categories: ")[1]
+            print("标签名为：%s" % tag.split("tag: ")[1])
+
             # 获取当天的日期
             today_date = time.strftime("%Y-%m-%d", time.localtime())
             # 获取现在的时间

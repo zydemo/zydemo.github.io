032101e7892e92f29389fa74fc1bca524076372e
zydemo <564359804@163.com>
2019-09-11 17:56:44 +0800

    Update script.js

--- styles/js/song/script.js
+++ styles/js/song/script.js
@@ -85,6 +85,7 @@ ogg: ''
 		audio.volume = localStorage.volume = value;
 		$('.volume .pace').css('width', value * 100 + '%');
 		$('.volume .slider a').css('left', value * 100 + '%');
+		$('.volume .slider a').removeClass('ui-slider-horizontal');
 	}
 
 	var volume = localStorage.volume || 0.5;

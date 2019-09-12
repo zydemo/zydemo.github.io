609fd7930b076b6f6ca9980275c36ab462758f6d
zydemo <564359804@163.com>
2019-09-12 18:34:50 +0800

    Update script.js

--- styles/js/song/script.js
+++ styles/js/song/script.js
@@ -182,28 +182,28 @@
             newaudio = $('<audio>').html('<source src="' + item.mp3 + '">').appendTo('#player');
         // 判断图片是否存在 https://www.cnblogs.com/hehaha/p/7266878.html
         url = item.cover;
-        // var xmlHttp;
-        // if (window.ActiveXObject) {
-        //     xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-        // }
-        // else if (window.XMLHttpRequest) {
-        //     xmlHttp = new XMLHttpRequest();
-        // }
-        // xmlHttp.open("Get", url, false);
-        // xmlHttp.send();
-        // if (xmlHttp.status === 404)
-        //     $('.cover').html('<img src="/styles/song_img/default.jpg" alt="' + item.album + '">');
-        // else
-        //     $('.cover').html('<img src="' + item.cover + '" title="' + item.title + '" alt="' + item.album + '">');
-
-        var ImgObj=new Image();
-        ImgObj.src= url;
-        if(ImgObj.fileSize > 0 || (ImgObj.width > 0 && ImgObj.height > 0))
-         {
-           $('.cover').html('<img src="' + item.cover + '" title="' + item.title + '" alt="' + item.album + '">');
-         } else {
-           $('.cover').html('<img src="/styles/song_img/default.jpg" alt="' + item.album + '">');
+        var xmlHttp;
+        if (window.ActiveXObject) {
+            xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
+        else if (window.XMLHttpRequest) {
+            xmlHttp = new XMLHttpRequest();
+        }
+        xmlHttp.open("Get", url, false);
+        xmlHttp.send();
+        if (xmlHttp.status === 404)
+            $('.cover').html('<img src="/styles/song_img/default.jpg" alt="' + item.album + '">');
+        else
+            $('.cover').html('<img src="' + item.cover + '" title="' + item.title + '" alt="' + item.album + '">');
+
+        // var ImgObj=new Image();
+        // ImgObj.src= url;
+        // if(ImgObj.fileSize > 0 || (ImgObj.width > 0 && ImgObj.height > 0))
+        //  {
+        //    $('.cover').html('<img src="' + item.cover + '" title="' + item.title + '" alt="' + item.album + '">');
+        //  } else {
+        //    $('.cover').html('<img src="/styles/song_img/default.jpg" alt="' + item.album + '">');
+        // }
 
         // 封面图
         // $('.cover').html('<img src="/styles/song_img/default.jpg" alt="'+item.album+'">');

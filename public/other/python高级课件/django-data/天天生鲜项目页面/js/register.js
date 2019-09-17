042183a79cca6ac6d0be2f4eae2455469abf42a8
zydemo <564359804@163.com>
2019-09-17 14:33:06 +0800

    Update register.js

--- public/other/python高级课件/django-data/天天生鲜项目页面/js/register.js
+++ public/other/python高级课件/django-data/天天生鲜项目页面/js/register.js
@@ -1,132 +1,99 @@
-$(function(){
+$(function () {
+    var error_name = false;
+    var error_pwd = false;
+    var error_cpwd = false;
+    var error_email = false;
+    var error_check = false;
 
-	var error_name = false;
-	var error_password = false;
-	var error_check_password = false;
-	var error_email = false;
-	var error_check = false;
+    $('#user_name').blur(function () {
+        check_user_name();
+    });
 
+    function check_user_name() {
+        var len = $('#user_name').val().length;
+        if (len < 5 || len > 20) {
+            error_name = true;
+            $('#user_name').next().show();
+            $('#user_name').next().html('请输入5-20个字符的用户名');
+        } else {
+            $('#user_name').next().hide();
+            error_name = false;
+        }
+    }
 
-	$('#user_name').blur(function() {
-		check_user_name();
-	});
+    $('#pwd').blur(function () {
+        check_pwd();
+        check_cpwd();
+    });
 
-	$('#pwd').blur(function() {
-		check_pwd();
-	});
+    function check_pwd() {
+        var len = $('#pwd').val().length;
+        if (len < 8 || len > 20) {
+            $('#pwd').next().show();
+            $('#pwd').next().html('密码最少8位，最长20位');
+            error_pwd = true;
+        } else {
+            $('#pwd').next().hide();
+            error_pwd = false;
+        }
+    }
 
-	$('#cpwd').blur(function() {
-		check_cpwd();
-	});
+    $('#cpwd').blur(function () {
+        check_cpwd();
+    });
 
-	$('#email').blur(function() {
-		check_email();
-	});
+    function check_cpwd() {
+        if ($('#pwd').val() !== $('#cpwd').val()) {
+            $('#cpwd').next().show();
+            $('#cpwd').next().html('两次输入的密码不一致');
+            error_cpwd = true;
+        } else {
+            $('#cpwd').next().hide();
+            error_cpwd = false;
+        }
+    }
 
-	$('#allow').click(function() {
-		if($(this).is(':checked'))
-		{
-			error_check = false;
-			$(this).siblings('span').hide();
-		}
-		else
-		{
-			error_check = true;
-			$(this).siblings('span').html('请勾选同意');
-			$(this).siblings('span').show();
-		}
-	});
+    $('#email').blur(function () {
+        check_email();
+    });
 
+    function check_email() {
+        var re = /^[a-z0-9A-Z][\w\-\.]*@[a-z0-9A-Z\-]+(\.[a-zA-Z]{2,5}){1,2}$/;
+        var result = re.test($('#email').val());
+        if (result) {
+            $('#email').next().hide();
+            error_email = false;
+        } else {
+            $('#email').next().show();
+            $('#email').next().html('你输入的邮箱格式不正确');
+            error_email = true;
+        }
+    }
 
-	function check_user_name(){
-		var len = $('#user_name').val().length;
-		if(len<5||len>20)
-		{
-			$('#user_name').next().html('请输入5-20个字符的用户名')
-			$('#user_name').next().show();
-			error_name = true;
-		}
-		else
-		{
-			$('#user_name').next().hide();
-			error_name = false;
-		}
-	}
+    $('#allow').click(function () {
+        if ($(this).is(':checked')) {
+            $(this).siblings('span').hide();
+            error_check = false;
+        } else {
+            $(this).siblings('span').show();
+            $(this).siblings('span').html('请勾选同意');
+            error_check = true;
+        }
+    });
 
-	function check_pwd(){
-		var len = $('#pwd').val().length;
-		if(len<8||len>20)
-		{
-			$('#pwd').next().html('密码最少8位，最长20位')
-			$('#pwd').next().show();
-			error_password = true;
-		}
-		else
-		{
-			$('#pwd').next().hide();
-			error_password = false;
-		}		
-	}
+    $('.reg_form').submit(function () {
+        check_user_name();
+        check_pwd();
+        check_cpwd();
+        check_email();
 
+        // if (error_name === false && error_pwd === false && error_cpwd === false && error_email === false && error_check === false) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        // 以上if-else简写为：
+        return (error_name === false && error_pwd === false && error_cpwd === false && error_email === false && error_check === false)
+    })
 
-	function check_cpwd(){
-		var pass = $('#pwd').val();
-		var cpass = $('#cpwd').val();
-
-		if(pass!=cpass)
-		{
-			$('#cpwd').next().html('两次输入的密码不一致')
-			$('#cpwd').next().show();
-			error_check_password = true;
-		}
-		else
-		{
-			$('#cpwd').next().hide();
-			error_check_password = false;
-		}		
-		
-	}
-
-	function check_email(){
-		var re = /^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$/;
-
-		if(re.test($('#email').val()))
-		{
-			$('#email').next().hide();
-			error_email = false;
-		}
-		else
-		{
-			$('#email').next().html('你输入的邮箱格式不正确')
-			$('#email').next().show();
-			error_check_password = true;
-		}
-
-	}
-
-
-	$('#reg_form').submit(function() {
-		check_user_name();
-		check_pwd();
-		check_cpwd();
-		check_email();
-
-		if(error_name == false && error_password == false && error_check_password == false && error_email == false && error_check == false)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-
-	});
-
-
-
-
-
-
-
-
-})
+});
